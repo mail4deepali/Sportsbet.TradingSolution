bbecb619cca250f6162c03e4292418ad7bc4a00f
DeepaliShah <mail4deepali@gmail.com>
2022-08-16 10:23:49 +1000

    Adding initial Solution

---
+++ Sportbet.TradingSolution/Models/Player.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Sportbet.TradingSolution.Models
+{
+    public class Player
+    {
+        public int Number { get; set; }
+        public string Name { get; set; }
+        public int Position { get; set; }
+    }
+}
